5f230196e80922ece4359e4c0bb3b3ee08dfc330
Tofuzhu <zzy19860808@gmail.com>
2017-12-10 15:08:31 +0800

    add test9-6

--- test9-6.py
+++ test9-6.py
@@ -25,8 +25,23 @@ print(kfc.number_served)
 kfc.number_served=28
 print(kfc.number_served)
 
-kfc.set_number_served()
-print("The number is "+str(kfc.number_served))
+#kfc.set_number_served()
+#print("The number is "+str(kfc.number_served))
 
 kfc.increment_number_served(99)
 print("The number is "+str(kfc.number_served))
+
+
+class IceCreamStand(Restaurant):
+    def __init__(self,restaurant_name,cuisine_type,number_served=0):
+        super().__init__(restaurant_name,cuisine_type,number_served)
+        self.name=restaurant_name
+        self.cuisine=cuisine_type
+        self.number_served=number_served
+        flavors=[]
+
+    def describe_icecreamstand(self):
+        print("the shop named "+self.name+" and it's "+self.cuisine+" and it served "+self.number_served+" peoples")
+
+dq=IceCreamStand("DQ","opened",100)
+dq.describe_restaurant()

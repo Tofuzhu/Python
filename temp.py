cc34e2a2601112cc79b365a0a5f0de40999b5ac0
Tofuzhu <zzy19860808@gmail.com>
2017-12-12 17:29:07 +0800

    modify

--- temp.py
+++ temp.py
@@ -1,8 +1,24 @@
 import json
 
-filename='username.json'
-with open(filename) as f_obj:
-    username=json.load(f_obj)
-    print(username)
+def get_stored_username():
+    filename='username.json'
+    try:
+        with open(filename,) as f_obj:
+            username=json.load(f_obj)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
 
+def greet_user():
+    username=get_stored_username()
+    if username:
+        print("Welcome back,"+username+"!")
+    else:
+        username=input("What is your name?")
+        filename='user'
+        with open(filename,"w") as f_obj:
+            json.dump(username,f_obj)
+            print("I remember you ! "+username)
 
+greet_user()

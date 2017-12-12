eaafed59cea061b197a711b88e110c5d88339e37
tofuzhu <zzy19860808@gmail.com>
2017-12-12 23:28:57 +0800

    finish chapter 10

--- test10-11.py
+++ test10-11.py
@@ -1,2 +1,14 @@
+import json
+
 def input_num():
-    input
+    a=input("Please enter your favorite number...")
+    filename='username.json'
+    with open(filename,"w") as ob:
+        json.dump(a,ob)
+    return filename
+
+
+filename=input_num()
+with open(filename) as ob:
+    number=json.load(ob)
+print("I know your favorite number is "+number)

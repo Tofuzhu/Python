379a48c4ca2f3039d2380b2a2cac424cb26e8d86
Tofuzhu <zzy19860808@gmail.com>
2017-12-12 11:34:39 +0800

    modify

---
+++ car.py
@@ -0,0 +1,28 @@
+"""a class used to show cars"""
+
+class Car():
+    def __init__(self,make,model,year):
+        self.make=make
+        self.model=model
+        self.year=year
+        self.odometer_reading=4000
+
+
+    def get_descriptive_name(self):
+        long_name=str(self.year)+' '+self.make+' '+self.model
+        return long_name.title()
+
+    def read_odometer(self):
+        print("This car has "+str(self.odometer_reading)+' miles on it.')
+
+    def update_odometer(self,mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading=mileage
+        else:
+            print("you are a cheater!")
+
+    def  increment_odometer(self,miles):
+        self.odometer_reading+=miles
+
+
+

38ad5164bdbbc2db8390e9b2e69e4d7fb9f203e2
Tofuzhu <zzy19860808@gmail.com>
2017-12-12 15:10:24 +0800

    modify

--- temp.py
+++ temp.py
@@ -1,14 +1,8 @@
-def count_words(file_name):
-    try:
-        with open(file_name) as file_object:
-            contents=file_object.read()
-    except FileNotFoundError:
-        print("Sorry,the file is not exist.")
-    else:
-        words=contents.split()
-        num_words=len(words)
-        print('The file '+file_name+' has about '+str(num_words)+'words.')
+import json
 
-file_name='alice.txt'
-count_words(file_name)
+filename='username.json'
+with open(filename) as f_obj:
+    username=json.load(f_obj)
+    print(username)
+
 

5961ac9897b3fea67a4b6c131beaec0c1a70b1c8
tofuzhu <zzy19860808@gmail.com>
2017-12-13 23:19:52 +0800

    finish chapter 10

--- test10-6.py
+++
@@ -1,8 +0,0 @@
-try:
-    a=int(input('input num a:'))
-    b=int(input('input num b:'))
-except ValueError:
-    print('You entered a wrong type data')
-else:
-    sum = a + b
-    print(sum)
--- test10-7.py
+++
@@ -1,9 +0,0 @@
-while True:
-    try:
-        a=int(input('input num a:'))
-        b=int(input('input num b:'))
-    except ValueError:
-        print('You entered a wrong type data')
-    else:
-        sum = a + b
-        print(sum)

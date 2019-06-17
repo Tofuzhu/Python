76f304bae80478d3b0935a16c69d71c34c19e968
Tofuzhu <zzy19860808@gmail.com>
2019-06-18 01:21:48 +0800

    qwe

--- demo/1.py
+++ demo/1.py
@@ -2,20 +2,23 @@ import requests
 import re
 
 r = requests.get(r"https://movie.douban.com/top250?start=0")
-w = open("1.txt",'w+',encoding="utf-8")
+w = open("1.txt", 'w+', encoding="utf-8")
 w.write(r.text)
-w2 = open("2.txt",'w+',encoding="utf-8")
 w.write(str(r.json))
 w.close()
-w2.close()
-
-pattern = re.compile(r"<span class=\"title\">(.*?)</span>")
-m = pattern.findall(r.text)
-print(m)
-n = m[:]
+# 获取本页的电影名称
+pattern_Name = re.compile(r"<span class=\"title\">(.*?)</span>")
+m = pattern_Name.findall(r.text)
+film_Name = m[:]
 for i in m:
     if "nbsp" in i:
-        n.remove(i)
-m = n
-# m = (i for i in m if "nbsp" not in i)
-print(n,len(n))
+        film_Name.remove(i)
+print(film_Name)
+# 获取本页的评分
+pattern_Rating = re.compile((r"<span class=\"rating_num\" property=\"v:average\">(.*?)</span>"))
+film_Rating = pattern_Rating.findall(r.text)
+print(film_Rating)
+# 获取观看人数
+pattern_People = re.compile((r"<span>(.*?)人评价</span>"))
+film_People = pattern_People.findall(r.text)
+print(film_People)
